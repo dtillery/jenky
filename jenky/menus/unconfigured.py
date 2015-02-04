a6456451cabf83c5f2ffad94e4d4b514639d6e73
David Tillery <dtillery@2u.com>
2015-02-03 21:22:42 -0500

    love that settings icon...

--- jenky/menus/unconfigured.py
+++ jenky/menus/unconfigured.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from workflow import ICON_SETTINGS
+
 from jenky.menus.base import BaseMenu
 
 class UnconfiguredMenu(BaseMenu):
@@ -16,6 +18,7 @@ class UnconfiguredMenu(BaseMenu):
                 "title": "Go to settings menu.",
                 "subtitle": "You'll need to configure username, api key, and hostname to get up and running.",
                 "valid": True,
-                "arg": "jenky-settings"
+                "arg": "jenky-settings",
+                "icon": ICON_SETTINGS
             }
         ]
